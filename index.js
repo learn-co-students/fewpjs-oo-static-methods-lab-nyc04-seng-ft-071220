1215511668c9e15fd5d83aaf74ecb16208d054a6
Nunny R <nunnyreyes@gmail.com>
2020-09-23 19:11:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 class Formatter {
   //add static methods here
+
+  
+    static sapitalize(string) {
+      return string.Capitalize()
+    }
+  
+    static sanitize(string) {
+      string.forEach()
+    }
+
+    static titleize() {
+      
+    }
 }
